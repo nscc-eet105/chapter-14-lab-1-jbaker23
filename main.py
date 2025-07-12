fd711e13eb22b8e9dc6828062e62d937dc6bfccd
Jacob Baker <jbaker23@students.northweststate.edu>
2025-07-12 14:30:44 -0400

    Assignment Complete

---
+++ main.py
@@ -0,0 +1,24 @@
+#My name is Jacob Baker and this is Chapter 14 Lab 1 being done on July 12
+import vehicle
+def main():
+    jacobscar = vehicle.Vehicle(0)
+
+    print("Accelerating...")
+    jacobscar.accelerate()
+    jacobscar.display_speed()
+
+    print("Accelerating...")
+    jacobscar.accelerate()
+    jacobscar.display_speed()
+
+    print("\nDecelerating...")
+    jacobscar.decelerate()
+    jacobscar.display_speed()
+
+    print("Decelerating...")
+    jacobscar.decelerate()
+    jacobscar.display_speed()
+
+if __name__ == "__main__":
+    main()
+
