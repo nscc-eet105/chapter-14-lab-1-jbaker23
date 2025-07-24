b972050edd9b28c8857fb9a37afe7ec1ad35f0dd
Jacob Baker <jbaker23@students.northweststate.edu>
2025-07-24 13:37:14 -0400

    Assignment Revised

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-#My name is Jacob Baker and this is Chapter 14 Lab 1 being done on July 12
 import vehicle
 def main():
-    jacobscar = vehicle.Vehicle(0)
-
-
+    jacobs_car = vehicle.Vehicle(0)
 
     print("Accelerating...")
-    jacobscar.accelerate()
-    jacobscar.display_speed()
+    jacobs_car.accelerate()
+    jacobs_car.display_speed()
 
     print("Accelerating...")
-    jacobscar.accelerate()
-    jacobscar.display_speed()
+    jacobs_car.accelerate()
+    jacobs_car.display_speed()
 
     print("\nDecelerating...")
-    jacobscar.decelerate()
-    jacobscar.display_speed()
+    jacobs_car.decelerate()
+    jacobs_car.display_speed()
 
     print("Decelerating...")
-    jacobscar.decelerate()
-    jacobscar.display_speed()
+    jacobs_car.decelerate()
+    jacobs_car.display_speed()
 
 if __name__ == "__main__":
     main()
--- vehicle.py
+++ vehicle.py
@@ -1,16 +1,16 @@
-#My name is Jacob Baker and this Chapter 14 Lab 1 done on July 12
+#My name is Jacob Baker and this Chapter 14 Lab 1 done on July 12. I am revising the code on July 24
 class Vehicle:
 
-    def __init__(self, speed):
-        self.speed = 0
+    def __init__(self):
+        self.__speed = 0
 
     def accelerate(self):
-        self.speed += 1
+        self.__speed += 1
 
     def decelerate(self):
-        self.speed -= 1
+        self.__speed -= 1
 
     def display_speed(self):
-        print(f"Current speed: {self.speed}")
+        print(f"Current speed: {self.__speed}")
 
 
